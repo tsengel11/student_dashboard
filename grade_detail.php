53fe296cffd8d4d3cfe0061b20f7ab3bfaa0c781
Admin <bayartsengelt@gmail.com>
2021-02-18 17:35:20 +1000

    created grade detail page

--- grade_detail.php
+++ grade_detail.php
@@ -9,3 +9,11 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 require_once(dirname(__FILE__) . '/../../config.php');
+global $DB;
+$user_id = $USER->id;
+
+$PAGE->set_url(new moodle_url('/blocks/student_dashboard/grade_detail.php'));
+$PAGE->set_context(\context_system::instance());
+$PAGE->set_title('Grade Details');
+
+echo $user_id;
