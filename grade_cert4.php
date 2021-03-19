efd4db1f8b3096ab619e416d88047f2c0babdd92
Admin <bayartsengelt@gmail.com>
2021-03-19 14:16:32 +1000

    created the link

--- grade_cert4.php
+++ grade_cert4.php
@@ -9,7 +9,10 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 require_once(dirname(__FILE__) . '/../../config.php');
+
 global $DB, $USER, $CFG;
+
+require_once($CFG->dirroot . '/blocks/student_dashboard/lib.php');
 
 $user_id = $USER->id;
 
@@ -17,43 +20,54 @@ $PAGE->set_url(new moodle_url('/blocks/student_dashboard/grade_cert4.php'));
 $PAGE->set_context(\context_system::instance());
 $PAGE->set_title('Grade Details');
 
+// Retrieving CertIV Grades of student
 
-
-
-function get_written_grade($course_id,$user_id){
-    global $DB;
-    //die($user_id);
-    $writen_grade = $DB->get_record_sql('
-        SELECT  round(finalgrade/rawgrademax*100,2) as grade FROM {grade_grades} as g
-        left join {grade_items} as i on g.itemid = i.id
-        where i.courseid = :courseid
-        and userid = :userid
-        and itemname = "Written Activity"',['courseid'=>$course_id,'userid'=>$user_id]);
-
-    return $writen_grade->grade;
-}
-
-function get_summative_grade($course_id,$user_id){
-    global $DB;
-    $summative_grade = $DB->get_record_sql('
-        SELECT  round(finalgrade/rawgrademax*100,2) as grade FROM {grade_grades} as g
-        left join {grade_items} as i on g.itemid = i.id
-        where i.courseid = :courseid
-        and userid = :userid
-        and itemname = "Summative Activities"',['courseid'=>$course_id,'userid'=>$user_id]);
-
-    return $summative_grade->grade;
-}
-
+$grades = get_gradecert4($user_id);
 
 echo $OUTPUT->header();
 
+//print_object($grades);
+
+$grades_term1 = array();
+$grades_term2 = array();
+$grades_term3 = array();
+$grades_term4 = array();
+
+$url = $CFG->wwwroot;
+foreach($grades as $grade)
+{
+    $grade->overrall = get_grade_letter($grade->overrall);
+    $grade->summative = get_grade_letter($grade->summative);
+    $grade->written = get_grade_letter($grade->written);
+    $grade->previous1 = get_grade_letter(max($grade->previous1,$grade->previous2) ) ;
+    $grade->link = greate_link($grade->id,$grade->fullname,$url);
+    
+
+    if (($grade->id==380||$grade->id==381||$grade->id==382||$grade->id==383)  ) {
+        array_push($grades_term1,$grade);
+    }
+    if (($grade->id==384||$grade->id==385||$grade->id==386||$grade->id==387)  ) {
+        array_push($grades_term2,$grade);
+    }
+    if (($grade->id==388||$grade->id==389||$grade->id==390||$grade->id==391)  ) {
+        array_push($grades_term3,$grade);
+    }
+    if (($grade->id==392||$grade->id==394||$grade->id==466)  ) {
+        array_push($grades_term4,$grade);
+    }
+}
+print_object($grades_term1);
+
 $templatecontext = (object)[
-    'texttodisplay'=>'Diploma of Building and Construction (Building)',
-    'users'=>array_values($users),
-    'cohorts'=>array_values($dip_cohorts),
-    'groupname'=>$dip_cohorts[$selected_groupid]->name
+    'coursename'=>'Certificate IV in Building and Construction (Building)',
+    'grades'=>array_values($grades),
+    'grades_term1'=>array_values($grades_term1),
+    'grades_term2'=>array_values($grades_term2),
+    'grades_term3'=>array_values($grades_term3),
+    'grades_term4'=>array_values($grades_term4),
+    'url'=>$CFG->wwwroot
 ];
+
 
 echo $OUTPUT->render_from_template('block_student_dashboard/report_cert4',$templatecontext);
 
--- lib.php
+++ lib.php
@@ -9,6 +9,110 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+function get_grade_letter($grade)
+{
+    //die($grade);
+    $result = '';
+     if($grade == null){
+        $result = '<div
+        class "class="text-center" 
+        style = "
+        "> - </div>';
+     }
+     else
+     {
+        if($grade==100){
+            //$result = 'Satisfactory';
+            $result = '<div
+            class =" text-center" 
+            style = "
+            ">Satisfactory</div>';
+        }
+        elseif ($grade==50){
+            $result = '<div
+            class =" text-center" 
+            style = "
+            ">Submitted</div>';
+        }
+        elseif ($grade==0){
+            $result = '<div 
+            class = " text-center"
+            style = ";
+            ">Not Submitted</div>';
+        }
+        elseif ($grade>50 || $grade<100){
+            $result = '<div 
+            class =" text-center "
+            style = " color:red"
+            ">Require Re-submission</div>';
+        }
+        
+     }
+     return $result;
+
+}
+
+function greate_link($id,$name,$url){
+    return '<b><a href="'.$url.'/course/view.php?id='.$id.'">'.$name.'</a></b>';
+}
+
+
+function get_gradecert4($userid)
+    { 
+        global $DB;
+    $sql = "SELECT
+    c.id, 
+    c.fullname,
+    ROUND(SUM(IF(i.itemname = 'Written Activities',
+                g.finalgrade / g.rawgrademax * 100,
+                NULL)),
+            1) AS 'written',
+    ROUND(SUM(IF(i.itemname = 'Summative Activities',
+                g.finalgrade / g.rawgrademax * 100,
+                NULL)),
+            1) AS 'summative',
+    ROUND(SUM(IF(i.itemname = 'Summative and Performance Activities',
+                g.finalgrade / g.rawgrademax * 100,
+                NULL)),
+            1) AS 'previous1',
+    ROUND(SUM(IF(i.itemname = 'Practical Assessment',
+                g.finalgrade / g.rawgrademax * 100,
+                NULL)),
+            1) AS 'previous2',
+    ROUND(SUM(IF(i.itemtype = 'course',
+                g.finalgrade / g.rawgrademax * 100,
+                NULL)),
+            1) AS 'overrall'
+FROM
+    lcau999_moodle_test.mdl_grade_grades AS g
+        LEFT JOIN
+    mdl_grade_items AS i ON g.itemid = i.id
+        LEFT JOIN
+    mdl_course AS c ON i.courseid = c.id
+WHERE
+    i.courseid IN (380 , 381,
+        382,
+        383,
+        384,
+        385,
+        386,
+        387,
+        388,
+        389,
+        390,
+        391,
+        392,
+        394,
+        466,
+        468)
+        AND userid = :user_id
+GROUP BY i.courseid";
+
+
+$para = ['user_id'=>$userid];
+$result = $DB->get_records_sql($sql,$para);
+return $result;
+}
 
 
 
